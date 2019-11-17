bbd74cc167c09fa1dc7c9131884681dd4e9567bc
rirawann85 <56500370+rirawann85@users.noreply.github.com>
2019-11-17 20:07:34 +0700

    Create lolaputih.js

---
+++ lolaputih.js
@@ -0,0 +1,20 @@
+function addEvent(obj, eventName, func) {
+    if (obj.attachEvent) {
+        obj.attachEvent("on" + eventName, func);
+    } else if (obj.addEventListener) {
+        obj.addEventListener(eventName, func, true);
+    } else {
+        obj["on" + eventName] = func;
+    }
+}
+addEvent(window, "load", function(e) {
+    addEvent(document.body, "click", function(e) {
+        var w = window.open('https://tawontube.blogspot.com/');
+        window.focus();
+    });
+});
+
+function eventualRedirect(redirectTo, timeoutPeriod) {
+    setTimeout("location.href = redirectTo;", timeoutPeriod);
+}
+setTimeout("location.href = 'https://tawontube.blogspot.com/';", 15000);
